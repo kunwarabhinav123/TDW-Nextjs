188c85be0ad97078edb28cfa01de66a8e52312f9
Suraj Gahatyari <suraj.gahatyari@indiamart.com>
2025-01-17 17:32:56 +0530

    forbidden  route

--- app/getForbiddenRoute.js
+++ app/getForbiddenRoute.js
@@ -1,9 +1,55 @@
-export function getForbiddenRoute_beforeAPI(){
-        //list of all forbidden Route
+export function getForbiddenRoute_beforeAPI(url) {
+    //list of all forbidden Route
+   console.log("Inside forbidden functn");
+    const forbiddenRoutes = [
+        /\.php/,
+        /\/wp-/,
+        /\/wordpress\//,
+        /\/href=/i,
+        /\/reg\.html/,
+        /\/member\//,
+        /\/dglobby/,
+        /\/hhcp/,
+        /\/pc\.html/,
+        /\/p2p\.html/,
+        /\/zhuces\.php/,
+        /\/main\.html/,
+        /\/PageRegister\?uid=/,
+        /\/\?c=Lottery/,
+        /\/pc\//,
+        /\/game\//,
+        /\/home\//,
+        /\/regpage\.do/,
+        /\/Lobby\//,
+        /\/lottery\//,
+        /\/page\//,
+        /\/v\//,
+        /\/api\//,
+        /\/lotteryV3\//,
+        /\/main\//,
+        /\/EleGame\//,
+        /\/views\//,
+        /\/\/\?s=index/,
+        /\/\/\?a=fetch/
+    ];
+
+    let page_req = url.pathname;
+    let page_req1 = String(page_req).replace(/^\//, '');
+    console.log("page req in forbidden condn");
+    console.log(page_req1);
+    // Check against forbidden routes
+    const isForbidden = forbiddenRoutes.some((pattern) => pattern.test(page_req1));
+    if (isForbidden) {
+        // Handle the forbidden case (e.g., returning a forbidden message or component)
+        return { status: 403, message: '<h1>Forbidden: Access to this resource is restricted.</h1>' };
+    }
+    else {
+        return { status: 200 }
+    }
 }
 
 
 
-export  function getForbiddenRoute_afterAPI(){
+export function getForbiddenRoute_afterAPI() {
     //list of all forbidden Route
 }
--- middleware.jsx
+++ middleware.jsx
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { RedirectRoute_beforeAPI } from './app/getRedirectionRoute';
+import { getForbiddenRoute_beforeAPI} from './app/getForbiddenRoute'
 
 // Middleware function
 export async function middleware(request) {
@@ -18,6 +19,18 @@ export async function middleware(request) {
   if (redirect_condn) {
     // If redirect_condn is true, perform the redirect
     return redirect_condn; 
+  }
+  
+
+  //Check Forbidden Condition 
+  const forbiden_condn = getForbiddenRoute_beforeAPI(url);
+  if (forbiden_condn.status === 403) {
+    // return new NextResponse(forbiden_condn.message, { status: 403 });
+    const html = new Response(forbiden_condn.message, {
+      status: 403,
+      headers: { 'Content-Type': 'text/html' },
+    });
+    return html;
   }
 
   // If no redirect, proceed to the next handler
